d406650867d2f766c70931ce0537a9c99bc48be9
Damir37 <damir.mullakhmetov.02@bk.ru>
2024-03-12 21:53:20 +0700

    обнов

--- LauncherClient/main.go
+++ LauncherClient/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"embed"
-
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
@@ -14,17 +13,25 @@ func main() {
 	app := NewApp()
 
 	err := wails.Run(&options.App{
-		Title:  "CoreCraft",
-		Width:  1024,
-		Height: 768,
+		Title:         "CoreCraft",
+		Width:         900,
+		Height:        500,
+		DisableResize: true,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
+		Frameless:        true,
+		CSSDragProperty:  "--wails-draggable",
+		CSSDragValue:     "drag",
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
 		},
+		//-------------------ПРИ СБОРКИ В Realse УБРАТЬ КОМЕНТ--------------------
+		//Windows: &windows.Options{
+		//WebviewUserDataPath: executableDir + "\\data\\",
+		//},
 	})
 
 	if err != nil {
